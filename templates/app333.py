d7889e54fef860327fe0e71bd39528030f85cd81
gcmastra <gcmastra@yahoo.com>
2021-09-05 21:33:02 -0400

    deliverable two completed code

--- templates/app333.py
+++ templates/app333.py
@@ -12,18 +12,16 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-   mars = mongo.db.mars.find_one()
+   mars = mongo.db.mars_app.find_one()
    # mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
    # mars = mongo.db.mars
-   mars = mongo.db.mars
+   mars = mongo.db.mars_app
    mars_data = scraping.scrape_all()
-   # mars.update({}, mars_data, upsert=True)
-   mars.replace_one({}, mars_data, upsert=True)
-
+   mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
 if __name__ == "__main__":
--- templates/scraping.py
+++ templates/scraping.py
@@ -20,10 +20,9 @@ def scrape_all():
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now(),
-        "images": mission_to_mars(browser)
+#        "featured_image": featured_image(browser),
+#        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
     }
 
     # Stop webdriver and return data
@@ -97,45 +96,9 @@ def mars_facts():
 
     return df.to_html()
 
-def mission_to_mars(browser):
 
-    hemisphere_image_urls = []
-
-    # 3. Write code to retrieve the image urls and titles for each hemisphere.
-    html = browser.html
-    mars_soup = soup(html, 'html.parser')
-
-
-    # hemisphere_dict = {"image":"placeholder". "title":"placeholder2"}
-
-    item_list = mars_soup.select('div.item')
-
-    for item in item_list:
-        next_part = item.find('a', class_='itemLink product-item').get('href')
-        item_title = item.find('h3').get_text()
-        url_second_page = f'{url}{next_part}'
-        # print(item_title)
-    
-        browser.visit(url_second_page)
-        html2 = browser.html
-        soup_page2 = soup(html2, 'html.parser')
-        img_url_rel2 = soup_page2.find('img', class_='wide-image').get('src')
-        # print(img_url_rel2)
-
-        img_url_full = f'{url}{img_url_rel2}'
-    
-        hemisphere_dict = {"image":"", "title":""}
-        hemisphere_dict["image"] = img_url_full
-        hemisphere_dict["title"] = item_title
-    
-        hemisphere_image_urls.append(hemisphere_dict)
-
-    browser.back()
-
-    return hemisphere_image_urls
-
-    
 if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
